e14b39c1b4914f5607c26f3bca68846ea318f717
Konrad Bojanecki <kbojanecki@gmail.com>
2025-08-02 10:57:27 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ function App() {
 
     return (
         <>
-            <h3>
+            <h1 className="text-3xl font-bold underline">
                 Hello world!
-            </h3>
+            </h1>
         </>
     )
 }
